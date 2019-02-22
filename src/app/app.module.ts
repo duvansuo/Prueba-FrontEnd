bc1289cc5523cbc7c42156e557e724c9f34aab29
Dario Sierra <dariosierra9@gmail.com>
2019-02-21 21:47:14 -0500

    order components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { MainSidebarComponent } from './components/shared/main-sidebar/main-sidebar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { CreateOrderComponent } from './components/order/create-order/create-order.component';
+import { ListOrderComponent } from './components/order/list-order/list-order.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainSidebarComponent,
-    FooterComponent
+    FooterComponent,
+    CreateOrderComponent,
+    ListOrderComponent
   ],
   imports: [
     BrowserModule,
