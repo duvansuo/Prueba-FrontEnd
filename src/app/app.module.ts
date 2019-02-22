8a3313d01d082c9a42ffdd733c9d3fb25ddd3dc3
Dario Sierra <dariosierra9@gmail.com>
2019-02-21 21:31:47 -0500

    components shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MainSidebarComponent } from './components/shared/main-sidebar/main-sidebar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainSidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
